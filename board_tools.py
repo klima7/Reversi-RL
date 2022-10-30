99bf2a254ff88cb00483151595dba30972a2471c
klima7 <ukasz.klimkiewicz@gmail.com>
2022-10-30 13:36:21 +0100

    Make board_tools absolute

--- board_tools.py
+++ board_tools.py
@@ -31,75 +31,75 @@ def inverted(board):
     return 1 - board
 
 
-def get_positions_to_reverse(rel_board, position):
+def get_positions_to_reverse(board, position, color):
     directions = [np.array([i, j]) for i in [-1, 0, 1] for j in [-1, 0, 1] if not (i == 0 and j == 0)]
     discs_to_reverse = []
 
     for direction in directions:
-        discs = get_positions_to_reverse_in_direction(rel_board, position, direction)
+        discs = get_positions_to_reverse_in_direction(board, position, color, direction)
         discs_to_reverse.extend(discs)
     return np.array(discs_to_reverse)
 
 
-def get_positions_to_reverse_in_direction(rel_board, position, direction):
+def get_positions_to_reverse_in_direction(board, position, color, direction):
     discs = []
     position = np.array(position) + direction
-    while is_valid_position(rel_board, position):
-        value = rel_board[position[0], position[1]]
-        if value == Side.ANY:
+    while is_valid_position(board, position):
+        value = board[position[0], position[1]]
+        if value == Color.ANY:
             return []
-        elif value == Side.OPPONENT:
-            discs.append(np.copy(position))
-        elif value == Side.ME:
+        elif value == color:
             return discs
+        else:
+            discs.append(np.copy(position))
         position += direction
     return []
 
 
-def is_valid_position(any_board, position):
-    return np.all(position >= 0) and np.all(position < any_board.shape[0])
+def is_valid_position(board, position):
+    return np.all(position >= 0) and np.all(position < board.shape[0])
 
 
-def get_legal_moves(rel_board):
-    empty_positions = np.column_stack(np.nonzero(rel_board == 0))
-    legal_position = [position for position in empty_positions if len(get_positions_to_reverse(rel_board, position)) > 0]
+def get_legal_moves(board, color):
+    empty_positions = np.column_stack(np.nonzero(board == Color.ANY))
+    legal_position = [position for position in empty_positions if len(get_positions_to_reverse(board, position, color)) > 0]
     return np.array(legal_position)
 
 
-def is_legal_move(rel_board, position):
-    return is_valid_position(rel_board, position) and \
-           rel_board[position[0], position[1]] == Side.ANY and \
-           len(get_positions_to_reverse(rel_board, position)) > 0
+def is_legal_move(board, position, color):
+    return is_valid_position(board, position) and \
+           board[position[0], position[1]] == Side.ANY and \
+           len(get_positions_to_reverse(board, position, color)) > 0
 
 
-def get_board_after_move(rel_board, position):
-    if not is_legal_move(rel_board, position):
+def get_board_after_move(board, position, color):
+    if not is_legal_move(board, position, color):
         raise Exception("Illegal move was requested")
-    new_rel_board = np.array(rel_board)
-    new_rel_board[position[0], position[1]] = Side.ME
-    reverse_positions = get_positions_to_reverse(rel_board, position)
-    new_rel_board[reverse_positions[:, 0], reverse_positions[:, 1]] = Side.ME
-    return new_rel_board
+    new_board = np.array(board)
+    new_board[position[0], position[1]] = Side.ME
+    reverse_positions = get_positions_to_reverse(board, position, color)
+    new_board[reverse_positions[:, 0], reverse_positions[:, 1]] = color
+    return new_board
 
 
-def is_finished(rel_board):
-    return is_board_full(rel_board) or no_one_has_moves(rel_board)
+def is_finished(board):
+    return is_board_full(board) or no_one_has_moves(board)
 
 
-def is_board_full(any_board):
-    return np.all(any_board != Side.ANY)
+def is_board_full(board):
+    return np.all(board != Color.ANY)
 
 
-def no_one_has_moves(rel_board):
-    return len(get_legal_moves(rel_board)) == 0 and len(get_legal_moves(inverted(rel_board))) == 0
+def no_one_has_moves(board):
+    return len(get_legal_moves(board, Color.WHITE)) == 0 and len(get_legal_moves(board, Color.BLACK)) == 0
 
 
-def get_winner(rel_board):
-    return np.sign(np.sum(rel_board))
+def get_winner(board):
+    return np.sign(np.sum(board))
 
 
-def plot(abs_board, title=None):
+def plot(board, title=None):
     cmap = ListedColormap(["black", "green", "white"], name='board', N=None)
-    plt.matshow(abs_board, cmap=cmap)
+    plt.matshow(board, cmap=cmap)
     plt.title(title)
     plt.show()
